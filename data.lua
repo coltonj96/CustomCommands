c880f6b1d4c424136ee99b660224758908bb32ee
coltonj96 <coltonjgriswold@aim.com>
2017-01-19 21:00:43 -0600

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,10 @@
+data:extend({
+  {
+    type = "custom-input",
+    name = "cc-toggle-gui",
+    key_sequence = "SHIFT + GRAVE",
+    consuming = "script-only"
+  }
+})
+
+require("prototypes.styles")
---
+++ prototypes/styles.lua
@@ -0,0 +1,6 @@
+data.raw["gui-style"]["default"]["cc-scroll-pane"] =
+{
+  type = "scroll_pane_style",
+  maximal_height = 400,
+  maximal_width = 600
+}
